02923769d8e58484548ab2790d32b5abf7567291
22520187 <146148909+22520187@users.noreply.github.com>
2024-05-05 20:12:56 +0700

    up

---
+++ app/src/main/java/com/example/cooking_social_network/Fragments/StartActivity.java
@@ -0,0 +1,58 @@
+package com.example.cooking_social_network.Fragments;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cooking_social_network.R;
+
+public class StartActivity extends AppCompatActivity {
+    private ImageView iconImage;
+    private LinearLayout linearLayout;
+    private Button register;
+    private Button login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        iconImage = findViewById(R.id.icon_image);
+        linearLayout = findViewById(R.id.liner_layout);
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
+
+        linearLayout.animate().alpha(0f).setDuration(10);
+
+        TranslateAnimation animation = new TranslateAnimation(0,0,0,-1000);
+        animation.setDuration(1000);
+        animation.setFillAfter(false);
+        animation.setAnimationListener(new MyAnimationListener());
+        iconImage.setAnimation(animation);
+    }
+
+    private class MyAnimationListener implements Animation.AnimationListener {
+        @Override
+        public void onAnimationStart(Animation animation) {
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            iconImage.clearAnimation();
+            iconImage.setVisibility(View.INVISIBLE);
+            linearLayout.animate().alpha(1f).setDuration(1000);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+        }
+    }
+
+}
